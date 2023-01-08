57a79a4214ff3e0792c4babcdb1ac9844605b5f4
shantanu sahay <71556064+shan0720-creator@users.noreply.github.com>
2023-01-08 11:50:37 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-const input = document.getElementById("numOfwords");
-const container = document.querySelector(".container");
-let numOfwords;
-const generateWord = (n) => {
-     let text = "";
-     const letter ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-     for(let i = 0;i<numOfwords;++i){
-          const random  = (Math.random()*25).toFixed(0);
-          text += letter[i];
-     }
-     
-     return text;
-};
-console.log(generateWord(5));
-const generatePara= () => {
-     numOfwords = Number(input.value);
-     const para = document.createElement("p");
-     let data = "";
-     for(let i = 0;i<10;++i){
-          const randomNumber = (Math.random()*15).toFixed(0);
-         data+= generateWord(randomNumber);
-         data+=" ";
-     }
-
-     para.innerText = data;
-     para.setAttribute("class","paras");
-     container.append(para);
-};
+let details = document.querySelector(".details");
+let imgContainer = document.querySelector(".img-container")
+let getUserBtn = document.getElementById("get-user-btn");
+let url = "https://random-data-api.com/api/v2/users?response_type=json";
 
 
+let getUser = () =>{
+    fetch(url).then((resp)=> resp.json()).then((data)=> {
+        console.log(data);
+        console.log(data.first_name);
+        console.log(data.last_name);
+        console.log(data.avatar);
+        console.log(data.employment.title)
+        console.log(data.address.city)
+
+        imgContainer.innerHTML = `<img src=${data.avatar}>`
+        details.innerHTML = `<h2>${data.first_name}${data.last_name}</h2>
+        <h3>${data.employment.title}</h3>
+        <h4><i class="fa-solid fa-location-dot"></i> ${data.address.city}</h4>`
+
+          let randomColor = "#" +((Math.random() * 0xffffff)<<0).toString(16).padStart(6,"0")
+          document.documentElement.style.setProperty("--theme-color",randomColor);
+    });
+}
+
+window.addEventListener("load",getUser);
+getUserBtn.addEventListener("click",getUser);
 
